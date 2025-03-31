4a43fed117b7dfb2207adcbbab725d1863f07bab
3Dbearnadette <eileenbearnadette@gmail.com>
2025-03-31 10:35:53 -0400

    laying groundwork for tags, save/load

---
+++ Tools/Note/scripts/note.gd
@@ -0,0 +1,19 @@
+extends Panel
+
+@onready var TitleEdit = $VBoxContainer/ScrollContainer/HBoxContainer/VBoxContainer/TitleEdit
+@onready var BodyEdit = $VBoxContainer/ScrollContainer/HBoxContainer/VBoxContainer/BodyEdit
+var tags : Array
+var title : String
+var body : String
+
+# when AddTag is pressed, create a line edit, expand_to_text_length true and connect the signal text_submitted().
+# when text_submitted(), make the text lowercase, remove all breakout characters, and add to the tag array.
+
+func _on_title_text_changed() -> void:
+	title = TitleEdit.text
+
+func _on_body_edit_text_changed() -> void:
+	body = BodyEdit.text
+
+# when Save is pressed, create a JSON containing the type (note), tags, title text, and body contents.
+# when Load is pressed, open a dialogue, find all JSONs of the type note, and load.
---
+++ scripts/searchandrun.gd
@@ -0,0 +1,11 @@
+extends PanelContainer
+
+@onready var InputLine = $VBoxContainer/LineEdit
+
+# model after rofi
+
+# Commands
+# !help
+# !roll [pass off to dice syntax]
+# !make [note, clock, turntracker, hexmap]
+# ?[query any tag]
