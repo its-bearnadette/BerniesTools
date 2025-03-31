ca3ddbd9e0d156c5ae4a0c78fb85503fd6459121
3Dbearnadette <eileenbearnadette@gmail.com>
2025-03-31 09:35:56 -0400

    fix diceroller still not working tho lol

--- Tools/DiceRoller/scripts/diceroller.gd
+++ Tools/DiceRoller/scripts/diceroller.gd
@@ -2,16 +2,16 @@ extends Panel
 
 @onready var ResultLabel = $VBoxContainer/ResultLabel
 @onready var OperandsLabel = $VBoxContainer/OperandsLabel
+@onready var InputRecord = $VBoxContainer/LineRecord
 @onready var InputLine = $VBoxContainer/InputLine
-@onready var InputRecord = $VBoxContainer/Label
 var rng = RandomNumberGenerator.new()
 
 func parse_command(input: String) -> void:
 	rng.randomize()
 	var result = dice_syntax.roll(input,rng)
-	print(result)
+	print(result.rolls)
 	ResultLabel.text = str(int(result.result))
-
+	#OperandsLabel.text = str(result.rolls.dice)
 func roll(sides) -> void:
 	rng.randomize()
 	var result = rng.randi_range(1,sides)
